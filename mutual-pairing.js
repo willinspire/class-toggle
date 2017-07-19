35f5d1e9f707d920494cdd541b3fb360a98f1a06
William Thompson <willinspire@users.noreply.github.com>
2017-07-19 04:32:51 -0400

    Update mutual-pairing.js

--- mutual-pairing.js
+++ mutual-pairing.js
@@ -1,6 +1,12 @@
-$('#foo2').click(function() {
-    $('#foo').toggleClass('active');
+var x;
+if (typeof x === 'undefined') {
+	$('#foo2').click(function() {
+    	$('#foo').toggleClass('active');
 });
-$('#foo').click(function() {
-    $('#foo2').toggleClass('active');
+}
+var x;
+if (typeof x === 'undefined') {
+	$('#foo').click(function() {
+    	$('#foo2').toggleClass('active');
 });
+}
